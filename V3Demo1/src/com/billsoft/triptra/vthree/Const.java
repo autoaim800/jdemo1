6238c1c78070b5af7d72af735d06cd5ddf65dc0b
Bill <autoaim800@gmail.com>
2013-11-19 17:37:21 +1100

    init v3 src

---
+++ V3Demo1/src/com/billsoft/triptra/vthree/Const.java
@@ -0,0 +1,18 @@
+package com.billsoft.triptra.vthree;
+
+import org.apache.log4j.Logger;
+
+public class Const {
+
+    public static final String DIST_ID = "Test_V3_Offload";
+    public static final String DIST_KEY = "60E0533B-6E38-4779-A7D7-0D82BF824C55";
+    public static final String CAT_ACCOMM = "Accommodation" ;
+    
+    public static final Logger logger = Logger.getLogger("com.billsoft.triptra");
+    public static final int SQL_DUPLICATE = 1062;
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+}
